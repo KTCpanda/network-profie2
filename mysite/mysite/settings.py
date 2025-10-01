209744dc48fd915ed6eb1e0b056ecb76d161fb42
KTCpanda <ktc24A31A0006@edu.kyoto-tech.ac.jp>
2025-10-01 16:48:22 +0900

    DEBUGをFalseに設定し、ALLOWED_HOSTSを更新

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bz6=rzqa+#n(y@o&($9b57gu-u4(yj1@qdxge4x5*04vfp+ey)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['network-profie2.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
