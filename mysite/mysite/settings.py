03cdbae8ffd25b6be9a8e63bcaebf82cc71c89be
KTCpanda <ktc24A31A0006@edu.kyoto-tech.ac.jp>
2025-10-01 17:23:14 +0900

    .envファイルを.gitignoreに追加し、settings.pyでdotenvを読み込むように修正

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv  # ← この行を追加
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, '.env')) # ← この行を追加
 
 
 # Quick-start development settings - unsuitable for production
