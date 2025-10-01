698e3dff5eae1ea50f194dd7c3fb94570ce245b2
KTCpanda <ktc24A31A0006@edu.kyoto-tech.ac.jp>
2025-10-01 17:47:18 +0900

    admin.pyのプロフィール管理セクションに空行を追加

--- mysite/profile_app/admin.py
+++ mysite/profile_app/admin.py
@@ -11,6 +11,8 @@ class ProfileAdmin(admin.ModelAdmin):
         ('画像設定', {
             'fields': ('top_image', 'icon_image', 'background_image')
         }),
+
+        
         ('プロフィール内容', {
             'fields': ('introduction', 'reason_for_vrc', 'hobbies', 'favorite_things', 'career_goal')
         }),
