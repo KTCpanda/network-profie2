276cb83cb0be7713a91512830a9792c15840bb2f
KTCpanda <ktc24A31A0006@edu.kyoto-tech.ac.jp>
2025-10-02 16:18:32 +0900

    プロフィール編集ビューのURLをコメントアウト

--- mysite/profile_app/urls.py
+++ mysite/profile_app/urls.py
@@ -6,5 +6,4 @@ app_name = 'profile_app'
 urlpatterns = [
     path('', views.profile_view, name='profile'),
     path('shumi/', views.shumi_view, name='shumi'),
-    # path('edit/', views.profile_edit_view, name='profile_edit'), # この行をコメントアウトします
 ]
