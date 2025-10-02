f1d6975214fdd16ffb0c490bfd3f73442d2558c6
KTCpanda <ktc24A31A0006@edu.kyoto-tech.ac.jp>
2025-10-02 13:47:29 +0900

    ホワイトノイズミドルウェアを追加し、静的ファイルの設定を更新

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,9 +134,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Media files (user uploaded files)
+# https://docs.djangoproject.com/en/5.2/topics/files/
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
