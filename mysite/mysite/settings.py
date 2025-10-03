3072528c0a94792aaf4a62229aa831cc5a29ea57
KTCpanda <ktc24A31A0006@edu.kyoto-tech.ac.jp>
2025-10-03 14:29:55 +0900

    .settings.pyにSECRET_KEYの設定を追加し、環境変数から取得するように変更しました .gitignoreから不要な行を削除しました

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -3,6 +3,17 @@
 import os
 from pathlib import Path
 
+# SECRET_KEY
+from decouple import config
+
+# ...
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# ...
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
